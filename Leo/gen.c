70e62025ea2bfac54657ab70824e5d53d9e34d47
Leo Tripier <leo.tripier@epita.fr>
2021-04-05 20:22:17 +0200

    premier push

---
+++ Leo/gen.c
@@ -0,0 +1,104 @@
+#include <err.h>
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+#include "pixel_operations.h"
+
+void init_sdl()
+{
+  // Init only the video part.
+  // If it fails, die with an error message.
+  if(SDL_Init(SDL_INIT_VIDEO) == -1)
+    errx(1,"Could not initialize SDL: %s.\n", SDL_GetError());
+}
+
+SDL_Surface* load_image(char *path)
+{
+  SDL_Surface *img;
+
+  // Load an image using SDL_image with format detection.
+  // If it fails, die with an error message.
+  img = IMG_Load(path);
+  if (!img)
+    errx(3, "can't load %s: %s", path, IMG_GetError());
+
+  return img;
+}
+
+SDL_Surface* display_image(SDL_Surface *img)
+{
+  SDL_Surface *screen;
+
+  // Set the window to the same size as the image
+  screen = SDL_SetVideoMode(img->w, img->h, 0, SDL_SWSURFACE|SDL_ANYFORMAT);
+  if (screen == NULL)
+    {
+      // error management
+      errx(1, "Couldn't set %dx%d video mode: %s\n",
+           img->w, img->h, SDL_GetError());
+    }
+
+  // Blit onto the screen surface
+  if(SDL_BlitSurface(img, NULL, screen, NULL) < 0)
+    warnx("BlitSurface error: %s\n", SDL_GetError());
+
+  // Update the screen
+  SDL_UpdateRect(screen, 0, 0, img->w, img->h);
+
+  // return the screen for further uses
+  return screen;
+}
+
+void wait_for_keypressed()
+{
+  SDL_Event event;
+
+  // Wait for a key to be down.
+  do
+    {
+      SDL_PollEvent(&event);
+    } while(event.type != SDL_KEYDOWN);
+
+  // Wait for a key to be up.
+  do
+    {
+      SDL_PollEvent(&event);
+    } while(event.type != SDL_KEYUP);
+}
+
+int* gen_img(float* mat, size_t rows, size_t cols, SDL_Surface* img)
+{
+  Uint32 pixel;
+
+  Uint8 r, g, b;
+
+  int* mat_next = malloc(rows * cols);
+
+  for(size_t i = 0;i < cols; i++)
+    {
+      for(size_t j = 0; j < rows; j++)
+        {
+          if(*(mat + (i * cols + j)) < (float)1.0)
+            {
+              pixel = SDL_MapRGB(image_surface->format, 46, 139, 87);
+              *(mat_next + (i*cols + j)) = 0;
+              //ocean
+            }
+          else if (*(mat + (i * cols + j)) < (float)2.0)
+            {
+              pixel = SDL_MapRGB(image_surface->format, 224, 205, 169);
+              *(mat_next + (i*cols + j)) = 1;
+              //sand
+            }
+          else
+            {
+              pixel = SDL_MapRGB(image_surface->format, 124, 252, 0);
+              *(mat_next + (i*cols + j)) = 2;
+              //ground
+            }
+
+          put_pixel(image_surface, i, j, pixel);
+        }
+    }
+
+  return mat_next;
+}
---
+++ Leo/interface.c
@@ -0,0 +1,118 @@
+#include <gtk/gtk.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+#include "pixel_operations.h"
+
+typedef struct UserInterface
+{
+  GtkWindow* window;             // Main window
+  GtkButton* start_button;        // Start button
+  GtkButton* close_button;       // Close button
+  GtkButton* with_seed_button;
+  GtkButton* get_seed_button;
+  GtkButton* gen_start_button;
+  GtkButton* gen_param_button;
+
+  GtkGrid* grid1;
+  GtkGrid* grid2;
+  GtkGrid* grid3;
+
+  SDL_Surface* image_surface;    //the map that will be created
+  GtkBuilder* builder;           //just to save the builder
+  GtkWidget* image;              //the image gtk will display (the map generated)
+
+} UserInterface;
+
+// Signal handler for the "clicked" signal of the buttons.
+void on_start(GtkButton *button,gpointer ui)
+{
+    UserInterface* i = ui;
+    (void)button;
+    gtk_container_remove(GTK_CONTAINER(i->window),GTK_WIDGET(i->grid1));
+    gtk_container_add(GTK_CONTAINER(i->window),GTK_WIDGET(i->grid2));
+}
+
+void on_with_seed(GtkButton *button,gpointer ui)
+{
+  UserInterface* i = ui;
+  (void)button;
+  gtk_container_remove(GTK_CONTAINER(i->window),GTK_WIDGET(i->grid2));
+}
+
+void on_get_seed(GtkButton *button,gpointer ui)
+{
+  UserInterface* i = ui;
+  (void)button;
+  (void)i;
+}
+
+void on_gen_start(GtkButton *button,gpointer ui)
+{
+  UserInterface* i = ui;
+  (void)button;
+  gtk_container_remove(GTK_CONTAINER(i->window),GTK_WIDGET(i->grid2));
+  gtk_container_add(GTK_CONTAINER(i->window),GTK_WIDGET(i->grid3));
+}
+
+void on_gen_param(GtkButton *button,gpointer ui)
+{
+  UserInterface* i = ui;
+  (void)button;
+  (void)i;
+}
+
+// Main function.
+int main()
+{
+    // Initializes GTK.
+    gtk_init(NULL, NULL);
+
+    // Loads the UI description and builds the UI.
+    // (Exits if an error occurs.)
+    GtkBuilder* builder = gtk_builder_new();
+    GError* error = NULL;
+    if (gtk_builder_add_from_file(builder, "interface.glade", &error) == 0)
+    {
+        g_printerr("Error loading file: %s\n", error->message);
+        g_clear_error(&error);
+        return 1;
+    }
+
+    //Getting the widgets
+    
+    UserInterface ui=
+      {
+       .window = GTK_WINDOW(gtk_builder_get_object(builder, "org.gtk.SeedWorld")),
+       .start_button = GTK_BUTTON(gtk_builder_get_object(builder, "start_button")),
+       .close_button = GTK_BUTTON(gtk_builder_get_object(builder, "close_button")),
+       .with_seed_button = GTK_BUTTON(gtk_builder_get_object(builder, "with_seed_button")),
+       .get_seed_button = GTK_BUTTON(gtk_builder_get_object(builder, "get_seed_button")),
+       .gen_start_button = GTK_BUTTON(gtk_builder_get_object(builder, "gen_start_button")),
+       .gen_param_button = GTK_BUTTON(gtk_builder_get_object(builder, "gen_param_button")),
+       .grid1 = GTK_GRID(gtk_builder_get_object(builder, "grido")),
+       .grid2 = GTK_GRID(gtk_builder_get_object(builder, "grido2")),
+       .grid3 = GTK_GRID(gtk_builder_get_object(builder, "grido3")),
+       .builder = builder,
+      };
+
+
+
+
+    // Connects signal handlers.
+    g_signal_connect(ui.window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    g_signal_connect(ui.start_button, "clicked", G_CALLBACK(on_start), &ui);
+    g_signal_connect(ui.close_button, "clicked", G_CALLBACK(gtk_main_quit), &ui);
+    g_signal_connect(ui.with_seed_button, "clicked", G_CALLBACK(on_with_seed), &ui);
+    g_signal_connect(ui.get_seed_button, "clicked", G_CALLBACK(on_get_seed), &ui);
+    g_signal_connect(ui.gen_start_button, "clicked", G_CALLBACK(on_gen_start), &ui);
+    g_signal_connect(ui.gen_param_button, "clicked", G_CALLBACK(on_gen_param), &ui);
+
+    // Runs the main loop.
+    gtk_main();
+
+    // Exits.
+    return 0;
+}
