b3bc530426fab9e7e9acb3fd6cae78ce300c47af
lea.cruciani <lea.cruciani@epita.fr>
2021-05-07 13:42:17 +0200

    .h upadated

--- city_generation/city_gen.h
+++ city_generation/city_gen.h
@@ -2,33 +2,11 @@
 #define CITY_GEN
 
 //======================FIND FIELD=========================
-//verify ifa city can be build
-int buildable(int* map, int x, int y, int cols, int rows, int width, int height);
 //find a position where a city can be build
 int* find_fields(int* map, int cols, int rows);
 
 //=====================BUILD ROADS========================
-//get a random number between min and max;
-int nb_rand(int min, int max);
-
-//define the direction of a road based on its orientation
-int define_dir(int x, int y, int cols, int rows, int vertical);
-
-//draw the road and returns its end
-int draw_road(int* map, int x, int y, int r_size, int cols, int rows, int vertical, int direction);
-
 //main fonction build all the roads of our future city
 void build_roads(int* map, int rows, int cols);
 
-//test if a future is possible to build
-int is_constructible(int*map,int x, int y, int len, int cols,  int vertical, int direction);
-
-//test if there is no other road near the future one
-int constructible(int*map, int x, int y,int fixed, int cols, int len, int i, int j, int vertical);
-
-//define the length of a road
-int define_len(int position, int max, int direction);
-
-//define the starting point of a road
-int define_begin(int position, int direction, int end);
 #endif
